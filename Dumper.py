a553f9d14566daf255282fd270d0ffa04666cc6a
Luca <33968601+LilWebDev@users.noreply.github.com>
2018-01-20 13:49:32 +0100

    Update Dumper.py

--- Dumper.py
+++ Dumper.py
@@ -1,16 +1,41 @@
 import os
+import shutil 
+import time
+import zipfile
+import sys
 
+try: color = sys.stdout.shell
+except AttributeError: raise RuntimeError("Use Python 3 or newer")
 
-print("Welcome to the Python System Dumper by iSn0w and JohnVS!")
+color.write("This tool was made by iSn0w and JohnVS!","KEYWORD")
 print("Soooo i think we'll have to say this ...")
 print("this is made for learning 'how-to-do-it' only, and not for any bad things...")
 print("first of all , please tell us your Operating system!")
 print("Right now, we are mainly working on Linux, Windows and MacOS will maybe come later !")
-OSC = input("macOS/Windows/Linux \n")
-if OSC =="Linux":
-    print("you've choosen Linux")
-    print("please tell us the path")
-    inspath = input("Path: ")
-for root, dirs, files in os.walk(inspath):  
-    for filename in files:
-        print(filename)
+startscr = input("type in start or help \n")
+if startscr == "help":
+    print("this is the help menu")
+    print("For making folders writable , start up the system Terminal and type in:")
+    print("Sudo CHMOD +X <Folder-Name>")
+elif startscr == "start":
+    OSC = input("macOS/Windows/Linux \n")
+    if OSC =="Linux":
+        print("you've choosen Linux")
+        inspath = input("specify the target directory: ")
+        dirlist = os.listdir(inspath)
+        from pprint import pprint
+        pprint(dirlist)
+    if os.access(inspath, os.W_OK) is not True:
+            print("Folder not writable, for a tutorial how to make it dumpable type in help when starting up the script")
+    else :
+            print("Folder writable! got full access to it")
+            print("now you are able to run some commands ! type in 'help' for a full list !")
+            navi = input()
+            if navi == "help":
+                print("Copy <File>     Copies a file to the clipboard")
+                print("Paste           Pastes a file from the clipboard")
+                print("Dump            brings you into the dumping menu")
+                print("cd              jumps in or out a folder")
+     
+                   
+    
